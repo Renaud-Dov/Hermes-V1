ed8e22d7e093b1d64d2031e8b531005e44dd1208
Dov Devers <contact@bugbear.fr>
2022-11-10 14:36:53 +0100

    fixed voice channel permissions

--- src/tickets.py
+++ src/tickets.py
@@ -10,6 +10,6 @@ async def create_private_channel(category: discord.CategoryChannel, student: dis
 
 async def create_vocal_channel(category: discord.CategoryChannel, student: discord.Member, name: str):
     """Creates a private channel for a student and an assistant"""
-    channel = await category.create_voice_channel(name=name)
-    await channel.set_permissions(student, connect=True, speak=True)
-    return channel
+    voice_channel = await category.create_voice_channel(name=name)
+    await voice_channel.set_permissions(student, speak=True)
+    return voice_channel
