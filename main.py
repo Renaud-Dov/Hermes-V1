0719dac71034869431075fa8e91f6d3a70a9520e
Dov Devers <contact@bugbear.fr>
2022-11-10 11:17:14 +0100

    test

--- main.py
+++ main.py
@@ -96,12 +96,13 @@ async def git(interaction: discord.Interaction):
 
 @tree.command(name="abel")
 async def abel(interaction: discord.Interaction):
+    await interaction.response.defer()
     if interaction.user.id != 208480161421721600:
         return
     embed = discord.Embed(title="Va bosser Chartier", color=discord.Color.dark_red())
     embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
     embed.set_image(url="https://media.tenor.com/SvQlro63ZscAAAAC/pas-content.gif")
-    await interaction.response.defer()
+
     await interaction.channel.send(embed=embed)
 
 
