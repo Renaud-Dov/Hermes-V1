0bc8e6e45cccc75b109dfefddb622195af99c16d
Dov Devers <renaud-dov.devers@epita.fr>
2023-03-16 08:05:20 +0100

    added commands section

---
+++ src/client/commands.py
@@ -0,0 +1,16 @@
+#  Copyright (c) 2023.
+#  Author: Dov Devers (https://bugbear.fr)
+#  All right reserved
+from discord.app_commands import Command
+
+from src.actions import links, tickets
+
+commands = [
+    {'command': Command(name="google", description="What do you know about `Let me google that for you`?",
+                        callback=links.google.google)},
+    {'command': Command(name="intra", description="Get the intra link", callback=links.intra.intra)},
+    {'command': Command(name="git", description="Get the git link", callback=links.git.git)},
+    {'command': Command(name="close", description="Mark a ticket as resolved", callback=tickets.close)},
+    {'command': Command(name="rename", description="Rename a ticket", callback=tickets.rename)},
+    {'command': Command(name="link", description="Link to another ticket", callback=links.link.link)},
+]
